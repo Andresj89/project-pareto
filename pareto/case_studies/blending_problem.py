387da7aecdafce9ca2ed67286b3c27b5795018a2
Andres Calderon <andresj_89@hotmail.com>
2021-12-14 22:22:49 -0500

    adding component material balances and simple solution strategy

--- pareto/case_studies/blending_problem.py
+++ pareto/case_studies/blending_problem.py
@@ -148,6 +148,21 @@ def create_model(df_sets, df_parameters):
         doc="Water production profiles in location l and time period t",
     )
 
+    model.p_prodcomp = Param(
+        model.s_L,
+        model.s_T,
+        default=0,
+        initialize=df_parameters["ProdComp"],
+        doc="Compostion at production sites l and time period t",
+    )
+
+    model.p_storeageinitcomp = Param(
+        model.s_L,
+        default=0,
+        initialize=df_parameters["StorageInitComp"],
+        doc="Compostion at storage sites l and time period t",
+    )
+
     # *******************************************************************************
     #                               VARIABLE DEFINITION
     # *******************************************************************************
@@ -207,6 +222,14 @@ def create_model(df_sets, df_parameters):
         model.s_T,
         within=NonNegativeReals,
         doc="Operational cost between location l and l' via transport mode g in time period t",
+    )
+
+    model.v_COMP = Var(
+        model.s_L,
+        model.s_T,
+        initialize=0,
+        within=NonNegativeReals,
+        doc="Composition at location l and time period t",
     )
 
     model.v_Z = Var(within=Reals, doc="Objective function variable [$]")
@@ -331,6 +354,64 @@ def create_model(df_sets, df_parameters):
         model.s_L, model.s_T, rule=material_balance_rule
     )
 
+    model.p_prodcomp
+
+    model.p_storeageinitcomp
+
+    # MATERIAL BALANCE PER COMPONENT
+    def component_material_balance_rule(m, l, t):
+        if t == model.s_T.first():
+            return (
+                m.v_P[l, t]*m.p_prodcomp[l,t]
+                + sum(
+                    sum(
+                        m.v_Q[l_tilde, g, l, t]*m.v_COMP[l_tilde,t]
+                        for l_tilde in m.s_L
+                        if m.p_topology[l_tilde, g, l]
+                    )
+                    for g in m.s_G
+                )
+                + m.p_init_storage[l]*m.p_storeageinitcomp[l]
+                == m.v_D[l, t]*m.v_COMP[l,t]
+                + sum(
+                    sum(
+                        m.v_Q[l, g, l_tilde, t]*m.v_COMP[l,t]
+                        for l_tilde in m.s_L
+                        if m.p_topology[l, g, l_tilde]
+                    )
+                    for g in m.s_G
+                )
+                + m.v_S[l, t]*m.v_COMP[l,t]
+            )
+
+        else:
+            return (
+                m.v_P[l, t]*m.p_prodcomp[l,t]
+                + sum(
+                    sum(
+                        m.v_Q[l_tilde, g, l, t]*m.v_COMP[l_tilde,t]
+                        for l_tilde in m.s_L
+                        if m.p_topology[l_tilde, g, l]
+                    )
+                    for g in m.s_G
+                )
+                + m.v_S[l, m.s_T.prev(t)]*m.v_COMP[l,m.s_T.prev(t)]
+                == m.v_D[l, t]*m.v_COMP[l,t]
+                + sum(
+                    sum(
+                        m.v_Q[l, g, l_tilde, t]*m.v_COMP[l,t]
+                        for l_tilde in m.s_L
+                        if m.p_topology[l, g, l_tilde]
+                    )
+                    for g in m.s_G
+                )
+                + m.v_S[l, t]*m.v_COMP[l,t]
+            )
+
+    model.component_material_balance = Constraint(
+        model.s_L, model.s_T, rule=component_material_balance_rule
+    )
+
     # FRESH WATER AVAILABILITY
     def fresh_water_rule(m, l, t):
         return m.v_P[l, t] <= m.p_freshwater[l, t]
@@ -415,6 +496,8 @@ if __name__ == "__main__":
         "TransportCost",
         "TransportDistances",
         "WaterProfiles",
+        "ProdComp",
+        "StorageInitComp"
     ]
 
     with resources.path("pareto.case_studies", "small_blending_problem.xlsx") as fpath:
@@ -423,34 +506,54 @@ if __name__ == "__main__":
     model = create_model(df_sets, df_parameters)
 
     solver = get_solver("gurobi_direct", "gurobi")
-    set_timeout(solver, timeout_s=60)
+    set_timeout(solver, timeout_s=60*10)
     solver.options["mipgap"] = 0
+    solver.options["NonConvex"] = 2
 
+    # Deactivate component material balance and solve for flow rates
+    model.component_material_balance.deactivate()
     results = solver.solve(model, tee=True)
 
-    # PRINTING RESULTS
+    # Fix flow rates and activate component material balance to POSTCALCULATE compositions
+    # model.v_P.fix()
+    # model.v_Q.fix()
+    # model.v_S.fix()
+    # model.v_D.fix()
 
+    # Or you can set a composition value
+    for i in model.v_COMP:
+        model.v_COMP[i].value = 50000
+
+    solver = get_solver("ipopt")
+    model.component_material_balance.activate()
+    results = solver.solve(model, tee=True)
+
+    # CREATE REPORT
     Q_results = []
     P_results = []
     D_results = []
     S_results = []
+    COMP_results = []
 
     Q_results = [(l, g, l_tilde, t, value(v)) for (l, g, l_tilde, t), v in model.v_Q.items()]
     P_results = [(l, t, value(v)) for (l, t), v in model.v_P.items()]
     D_results = [(l, t, value(v)) for (l, t), v in model.v_D.items()]
     S_results = [(l, t, value(v)) for (l, t), v in model.v_S.items()]
+    COMP_results = [(l, t, value(v)) for (l, t), v in model.v_COMP.items()]
 
     Q_df = pd.DataFrame(Q_results, columns=["origin","mode", "destination","time","variable value"])
     P_df = pd.DataFrame(P_results, columns=["origin", "time", "variable value"])
     D_df = pd.DataFrame(D_results, columns=["origin", "time", "variable value"])
     S_df = pd.DataFrame(S_results, columns=["origin", "time", "variable value"])
+    COMP_df = pd.DataFrame(COMP_results, columns=["origin", "time", "variable value"])
 
 
-    with pd.ExcelWriter("results.xlsx") as writer:
+    with pd.ExcelWriter("blendin_problem_results.xlsx") as writer:
         Q_df.to_excel(writer, sheet_name="Transfers")
         P_df.to_excel(writer, sheet_name="Production")
         D_df.to_excel(writer, sheet_name="Demand")
         S_df.to_excel(writer, sheet_name="Storage")
+        COMP_df.to_excel(writer, sheet_name="Composition")
 
     print("end")
 
